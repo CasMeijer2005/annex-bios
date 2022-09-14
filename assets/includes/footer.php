f9029274b53983a0c684e5ef9bc5b373a3bfe358
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-09-14 10:41:51 +0200

    php includes

---
+++ assets/includes/footer.php
@@ -0,0 +1,40 @@
+<footer>
+    <ul>
+        <a href="/index.html">
+            <img id="page-logo" src="assets/img/AnnexBios-white.png" alt="AnnexBios logo">
+        </a>
+        <li>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid fugit tempore nisi laudantium repellendus maxime quibusdam amet inventore deleniti eaque.</h2>
+        </li>
+        <li><button>LEES MEER</button></li>
+    </ul>
+    <ul>
+        <li>
+            <h2>NAVIGEER</h2>
+        </li>
+        <li>
+            <h4>Veelgestelde vragen</h4>
+        </li>
+        <li>
+            <h4>Werken bij</h4>
+        </li>
+        <li>
+            <h4>Vestigingen</h4>
+        </li>
+        <li>
+            <h4>Contact</h4>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <h2>VOLG ONS</h2>
+        </li>
+        <li>
+            <img src="assets/img/facebook.png">
+            <img src="assets/img/twitter.png">
+            <img src="assets/img/instagram.png">
+
+        </li>
+    </ul>
+    <img id="filmlijn" src="assets/img/filmlijn.png">
+</footer>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include "assets/includes/db.php";
+include "assets/includes/head.php";
+
+include 'assets/includes/header.php'; ?>
+<div class="bgn">
+    <div class="blok1">
+        <div class="welkom-text">
+            <h3 id="welkom-2">WELKOM BIJ ANNEXBIOS BILTHOVEN</h3>
+            <h3 id="if">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient</h3>
+            <button id="btn-kies">BEKIJK DE DRAAIENDE FILMS</button>
+        </div>
+    </div>
+    <div class="film-agenda">
+    </div>
+    <div class="blok2">
+        <ul><iframe id="google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1232.6981619616763!2d4.132889234987656!3d51.83545731900257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c451c6f4434d53%3A0x20bb4b6bcdd57904!2sRijksstraatweg%2042%2C%203223%20KA%20Hellevoetsluis!5e0!3m2!1snl!2snl!4v1663063420458!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="tekst2">
+                <ul>
+                    <img id="location" src="assets/img/location.png">
+                    <h3 id="tekst3"> Rijksstraatweg 42 <br>
+                        3223 KA Hellevoetsluis</h3>
+                    <img id="phone" src="assets/img/phone.png">
+                    <h3 id="tekst3"> 020-12345678
+
+                </ul>
+            </div>
+        </ul>
+        <img id="Helle" src="assets/img/Hellevoetssluis.png">
+
+
+    </div>
+</div>
+<?php include 'assets/includes/footer.php' ?>
+</body>
+
+</html>
