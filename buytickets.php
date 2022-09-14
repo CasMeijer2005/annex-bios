85a7b3c247a772ce7ccc0be3dac2d5256b7f42e8
Michael <michaelzhang5654@gmail.com>
2022-09-14 12:10:48 +0200

    Buy tickets page initial

---
+++ buytickets.php
@@ -0,0 +1,17 @@
+<?php
+include "assets/includes/db.php";
+include "assets/includes/head.php";
+include "assets/includes/header.php"
+?>
+
+<main>
+    <div>
+        <h1>Tickets Bestellen</h1>
+    </div>
+    <div>
+        
+        <input type="date">
+    </div>
+</main>
+
+<?php include "assets/includes/footer.php"; ?>
--- ticket.php
+++
@@ -1,6 +0,0 @@
-<?php include "assets/includes/db.php";
-include "assets/includes/head.php";
-include "assets/includes/header.php";
-?>
-<form action=""></form>
-include "assets/includes/footer.php";
