6bcccfbff65d63733ed31d8d96ef41da2bec60ba
mintyCW <210357@student.glu.nl>
2022-09-19 00:59:07 +0200

    ticket page update

--- buytickets.php
+++ buytickets.php
@@ -95,18 +95,113 @@ include "assets/includes/header.php"
                                 <input id="voucher-button" type="submit" value="Toevoegen">
                             </div>
                         </div>
-                    </div>
-                    <div class="order-seat-placement">
-                        <img src="assets/img/zaal_paars.png" alt="Zaal img">
-                        <div class="order-seat-buttons">
-                            <input id="voucher-button" type="submit" value="Vrij">
-                            <input id="voucher-button" type="submit" value="Bezet">
-                            <input id="voucher-button" type="submit" value="Jouw Selectie">
+                        <div class="choose-seats">
+                            <div class="buy-steps">
+                                <h4>Stap 2: Kies je stoel</h4>
+                            </div>
+                            <div class="order-seat-placement">
+                                <img src="assets/img/zaal_paars.png" alt="Zaal img">
+                                <div class="order-seat-buttons">
+                                    <input id="available-button" type="submit" value="Vrij">
+                                    <input id="unavailable-button" type="submit" value="Bezet">
+                                    <input id="selected-button" type="submit" value="Jouw Selectie">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="order-info">
+                            <div class="buy-steps">
+                                <h4>Stap 3: Controleer je bestelling</h4>
+                            </div>
+                            <div class="order-info-border">
+                                <div class="order-info-content">
+                                    <img class="order-info-poster" src="assets/img/jurassicpark poster.png" alt="filmposter">
+                                    <div>
+                                        <div class="order-ticket-info">
+                                            <h3 class="order-ticket-title">Jurassic World: Fallen Kingdom</h3>
+                                            <div class="order-ticket-film-restrictions">
+                                                <img src="assets/img/kijkwijzer-12.png" alt="kijkwijzer 12">
+                                                <img src="assets/img/kijkwijzer-eng.png" alt="kijkwijzer eng">
+                                                <img src="assets/img/kijkwijzer-geweld.png" alt="kijkwijzer geweld">
+                                            </div>
+                                            <div>
+                                                <ul class="order-ticket">
+                                                    <li>
+                                                        <strong class="additional-info-header">Bioscoop: </strong>
+                                                        <p>Hellevoetsluit (Zaal 3)</p>
+                                                    </li>
+                                                    <li>
+                                                        <strong class="additional-info-header">Wanner: </strong>
+                                                        <p>11 juni 14:15</p>
+                                                    </li>
+                                                    <li>
+                                                        <strong class="additional-info-header">Stoelen: </strong>
+                                                        <p>Rij 2, stoel 7</p>
+                                                    </li>
+                                                    <li>
+                                                        <strong class="additional-info-header">Tickets: </strong>
+                                                        <p>1x normaal</p>
+                                                    </li>
+                                                    <li class="order-ticket-total">
+                                                        <strong class="additional-info-header">Totaal 1 ticket: </strong>
+                                                        <p>&#8364; 9,00</p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="buy-steps">
+                                    <h4>Stap 4: Vul je gegevens in</h4>
+                                </div>
+                                <div class="data-grid">
+                                    <input class="name-input" type="text" value="Voornaam">
+                                    <input class="name-input" type="text" value="Achternaam*">
+                                    <input class="email-input" type="text" value="E-mailadres*">
+                                    <input class="email-input" type="text" value="E-mailadres*">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="buy-steps">
+                                    <h4>Stap 5: Kies je betaalwijze</h4>
+                                </div>
+                                <div>
+                                    <ul class="payment-choice">
+                                        <li>
+                                            <input type="checkbox" id="gift-card" name="gift-card" value="BioscoopBon">
+                                            <label for="gift-card">
+                                                <img src="assets/img/nationale-bioscoop-bon.png" alt="Bioscoop bon foto">
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="maestro" name="maestro" value="Maestro">
+                                            <label for="gift-card">
+                                                <img src="assets/img/maestro.png" alt="Bioscoop bon foto">
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="ideal" name="ideal" value="IDeal">
+                                            <label for="gift-card">
+                                                <img src="assets/img/ideal.png" alt="Bioscoop bon foto">
+                                            </label>
+                                        </li>
+                                    </ul>
+                                    <div class="tos-container">
+                                        <input type="checkbox" id="tos" name="tos" value="TermsOfService">
+                                        <label for="tos">
+                                            <p>
+                                                Ja, ik ga akkoord met de <a id="tos-text" href="index.php" target="_blank">algemene voorwaarden</a>
+                                            </p>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <input class="buy-ticket" type="submit" value="Afrekenen">
             </div>
-            <input class="buy-tickets btn" type="submit" value="Afrekenen">
         </div>
     </form>
 </main>
