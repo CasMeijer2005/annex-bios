8e8887e9ab0270aa32484a9e410491ffe67ce6ae
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-09-19 12:55:26 +0200

    update

---
+++ index.php
@@ -0,0 +1,201 @@
+<?php include "assets/includes/head.php" ?>
+<header>
+    <nav>
+        <a href="/index.html">
+            <img id="page-logo" src="/assets/img/AnnexBios Logo.png" alt="AnnexBios logo">
+        </a>
+        <ul>
+            <li>
+                <h2>Film Agenda</h2>
+            </li>
+            <li>
+                <h2>Alle Vestigingen</h2>
+            </li>
+            <li>
+                <h2>Contact</h2>
+            </li>
+        </ul>
+    </nav>
+    <div id="header">
+        <div id="header-container">
+            <h2>Koop je Tickets</h2>
+            <select id="header-select" name="movies" id="movies">
+                <option value="Filler" selected>Kies je film</option>
+                <option value="JurassicPark">Jurassic Park: Fallen Kingdom</option>
+                <option value="DeadPool2">DeadPool 2</option>
+                <option value="Solo">Solo: A Star Wars Story</option>
+            </select>
+            <a href="/index.html">Bestel Tickets</a>
+        </div>
+
+    </div>
+    </div>
+    <div class="film-agenda">
+    </div>
+    <div class="blok2">
+        <ul><iframe id="google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1232.6981619616763!2d4.132889234987656!3d51.83545731900257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c451c6f4434d53%3A0x20bb4b6bcdd57904!2sRijksstraatweg%2042%2C%203223%20KA%20Hellevoetsluis!5e0!3m2!1snl!2snl!4v1663063420458!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="tekst2">
+                <ul>
+                    <img id="location" src="assets/img/location.png">
+                    <h3 id="tekst3"> Rijksstraatweg 42 <br>
+                        3223 KA Hellevoetsluis</h3>
+                    <img id="phone" src="assets/img/phone.png">
+                    <h3 id="tekst3"> 020-12345678
+                        <div id="text4">
+                            <h2 id="t4">Bereikbaarheid</h2>
+                            <h4 id="t5" t>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem ab maxime
+                                ducimus assumenda praesentium corrupti expedita placeat eveniet explicabo provident?
+                            </h4>
+                        </div>
+
+
+                </ul>
+            </div>
+        </ul>
+        <img id="Helle" src="assets/img/Hellevoetssluis.png">
+
+
+    </div>
+    </div>
+    <div class="bgn2">
+        <div class="filmag">
+            <h2 id="filmag1">FILMAGENDA</h2>
+        </div>
+        <div class="menu2">
+            <div id="films"><input type="radio" id="films">
+                <label for="films">FILMS</label>
+            </div>
+
+            <div id="week"><input type="radio" id="week">
+                <label for="vandaag">DEZE WEEK</label>
+            </div>
+
+            <div id="vandaag"><input type="radio" id="vandaag">
+                <label for="vandaag">VANDAAG</label>
+            </div>
+
+            <select id="Category" name="movies" id="Category">
+                <option value="Filler" selected id="category">Category</option>
+                <option value="JurassicPark">Jurassic Park: Fallen Kingdom</option>
+                <option value="DeadPool2">DeadPool 2</option>
+                <option value="Solo">Solo: A Star Wars Story</option>
+            </select>
+        </div>
+        <div class="flex">
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+            <div class="filmposter">
+                <img id="poster" src="assets/img/JurassicPark Poster.png">
+                <div id="textfilm">
+                    <h2 id="tf">JURASSIC WORLD: Fallen Kingdom</h2>
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <img id="star" src="assets/img/Path 23.png">
+                    <h4 id="tf2">Release: 7-06-2018
+                    </h4>
+                    <h4 id="tf3">Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit
+                        3D actie-spektakel.</h4>
+                    <button id="btn-draaiende">MEER INFO & TICKETS</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include 'assets/includes/footer.php';
--- ticket.php
+++
@@ -1,7 +0,0 @@
-<?php include "assets/includes/db.php";
-include "assets/includes/head.php";
-include "assets/includes/header.php";
-?>
-<form action=""></form>
-<?php
-include "assets/includes/footer.php";
